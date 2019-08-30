347b5b34cef93a38d9e6576dfac6d03b315eea0d
gin <ginjirokimura0831@gmail.com>
2019-08-30 06:37:53 +0000

    Finish chapter 4

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -2,68 +2,81 @@ require 'rails_helper'
 
 RSpec.describe Note, type: :model do
   
-  before do
-    @user = User.create(
-      first_name: "Joe",
-      last_name: "Tester",
-      email: "joetester@example.com",
-      password: "dottle-nouveau-pavilion-tights-furze"
-    )
+  it "generates associated date from a factory" do
+    note = FactoryBot.create(:note)
     
-    @project = @user.projects.create(
-      name: "Test Project"
-    )
+    puts "This note's project is #{note.project.inspect}"
+    puts "This note's user is #{note.user.inspect}"
   end
   
-  it "is valid with a user, project, and message" do
-    note = Note.new(
-      message: "This is a sample note.",
-      user: @user,
-      project: @project
-    )
-    
-    expect(note).to be_valid
-  end
+  # context before 練習
   
-  it "is invalid without a message" do
-    note = Note.new(message: nil)
+  # before do
+  #   @user = User.create(
+  #     first_name: "Joe",
+  #     last_name: "Tester",
+  #     email: "joetester@example.com",
+  #     password: "dottle-nouveau-pavilion-tights-furze"
+  #   )
     
-    note.valid?
-    
-    expect(note.errors[:message]).to include("can't be blank")
-  end
+  #   @project = @user.projects.create(
+  #     name: "Test Project"
+  #   )
+  # end
   
-  describe "search message for a term" do
+  # it "is valid with a user, project, and message" do
+  #   note = Note.new(
+  #     message: "This is a sample note.",
+  #     user: @user,
+  #     project: @project
+  #   )
     
-    before do
-      @note1 = @project.notes.create(
-        message: "This is the first note.",
-        user: @user
-      )
+  #   expect(note).to be_valid
+  # end
+  
+  # it "is invalid without a message" do
+  #   note = Note.new(message: nil)
+    
+  #   note.valid?
+    
+  #   expect(note.errors[:message]).to include("can't be blank")
+  # end
+  
+  # describe "search message for a term" do
+    
+  #   before do
+  #     @note1 = @project.notes.create(
+  #       message: "This is the first note.",
+  #       user: @user
+  #     )
       
-      @note2 = @project.notes.create(
-        message: "Thisis the second note.",
-        user: @user
-      )
+  #     @note2 = @project.notes.create(
+  #       message: "Thisis the second note.",
+  #       user: @user
+  #     )
       
-      @note3 = @project.notes.create(
-        message: "First, preheat the oven.",
-        user: @user
-      )
-    end
+  #     @note3 = @project.notes.create(
+  #       message: "First, preheat the oven.",
+  #       user: @user
+  #     )
+  #   end
     
-    context "when a match is found" do
-      it "returns notes that match the search term" do
-        expect(Note.search("first")).to include(@note1, @note3)
-      end
-    end
+  #   context "when a match is found" do
+  #     it "returns notes that match the search term" do
+  #       expect(Note.search("first")).to include(@note1, @note3)
+  #     end
+  #   end
     
-    context "when no match is found" do
-      it "results an empty collection" do
-        expect(Note.search("message")).to be_empty
-      end
-    end
-  end
+  #   context "when no match is found" do
+  #     it "results an empty collection" do
+  #       expect(Note.search("message")).to be_empty
+  #     end
+  #   end
+  # end
+  
+  # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  
+  # 普通
   
   # it "returns notes that match the search term" do
   #   user = User.create(
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,65 +1,167 @@
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
-  it "does not allow duplicate project names per user" do
-    user = User.create(
-      first_name: "Joe",
-      last_name:  "Tester",
-      email:      "joetester@example.com",
-      password:   "dottle-nouveau-pavilion-tights-furze"
-    )
+  
+  # コールバック
+  
+  it "can have many notes" do
+    project = FactoryBot.create(:project, :with_notes)
     
-    user.projects.create(
-      name: "Test Project"
-    )
-    
-    new_project = user.projects.build(
-      name: "Test Project"
-    )
-    
-    new_project.valid?
-    
-    expect(new_project.errors[:name]).to include("has already been taken")
+    expect(project.notes.length).to eq 5
   end
   
-  it "allow two users to share a project name" do
-    user = User.create(
-      first_name: "Joe",
-      last_name:  "Tester",
-      email:      "joetester@example.com",
-      password:   "dottle-nouveau-pavilion-tights-furze"
-    )
-    
-    user.projects.create(
-      name: "Test Project"
-    )
-    
-    other_user = User.create(
-      first_name: "Jane",
-      last_name:  "Tester",
-      email:      "janetester@example.com",
-      password:   "dottle-nouveau-pavilion-tights-furze"
-    )
-    
-    other_project = other_user.projects.build(
-      name: "Test Project"
-    )
-    
-    expect(other_project).to be_valid
-  end
+  # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   
-  it 'is invalid without a project name' do
-    user = User.create(
-      first_name: "Joe",
-      last_name:  "Tester",
-      email:      "joetester@example.com",
-      password:   "dottle-nouveau-pavilion-tights-furze"
-    )
+  # トレイト
+  
+  # it "is late when the due date is past today" do
+  #   project = FactoryBot.create(:project, :due_yesterday)
     
-    project = user.projects.new(name: nil)
+  #   expect(project).to be_late
+  # end
+  
+  # it "is on time when the due date is today" do
+  #   project = FactoryBot.create(:project, :due_today)
     
-    project.valid?
+  #   expect(project).to_not be_late
+  # end
+  
+  # it "is on time when the due date is in the future" do
+  #   project = FactoryBot.create(:project, :due_tomorrow)
     
-    expect(project.errors[:name]).to include("can't be blank")
-  end
+  #   expect(project).to_not be_late
+  # end
+  
+  # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  
+  # 継承
+  
+  # it "is late when the due date is past today" do
+  #   project = FactoryBot.create(:project_due_yesterday)
+    
+  #   expect(project).to be_late
+  # end
+  
+  # it "is on time when the due date is today" do
+  #   project = FactoryBot.create(:project_due_today)
+    
+  #   expect(project).to_not be_late
+  # end
+  
+  # it "is on time when the due date is in the future" do
+  #   project = FactoryBot.create(:project_due_tomorrow)
+    
+  #   expect(project).to_not be_late
+  # end
+  
+  # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  
+  # 演習
+  
+  # it "does not allow duplicate project names per user" do
+  #   user = FactoryBot.create(:user)
+    
+  #   user.projects.create(
+  #     name: "Test Project"
+  #   )
+    
+  #   new_project = user.projects.build(
+  #     name: "Test Project"
+  #   )
+    
+  #   new_project.valid?
+    
+  #   expect(new_project.errors[:name]).to include("has already been taken")
+  # end
+  
+  #   it "allow two users to share a project name" do
+  #   user = FactoryBot.create(:user)
+    
+  #   user.projects.create(
+  #     name: "Test Project"
+  #   )
+    
+  #   other_user = FactoryBot.create(:user)
+    
+  #   other_project = other_user.projects.build(
+  #     name: "Test Project"
+  #   )
+    
+  #   expect(other_project).to be_valid
+  # end
+  
+   
+  # it 'is invalid without a project name' do
+  #   user = FactoryBot.create(:user)
+    
+  #   project = user.projects.new(name: nil)
+    
+  #   project.valid?
+    
+  #   expect(project.errors[:name]).to include("can't be blank")
+  # end
+  
+  # 普通
+  
+  # it "does not allow duplicate project names per user" do
+  #   user = User.create(
+  #     first_name: "Joe",
+  #     last_name:  "Tester",
+  #     email:      "joetester@example.com",
+  #     password:   "dottle-nouveau-pavilion-tights-furze"
+  #   )
+    
+  #   user.projects.create(
+  #     name: "Test Project"
+  #   )
+    
+  #   new_project = user.projects.build(
+  #     name: "Test Project"
+  #   )
+    
+  #   new_project.valid?
+    
+  #   expect(new_project.errors[:name]).to include("has already been taken")
+  # end
+  
+  # it "allow two users to share a project name" do
+  #   user = User.create(
+  #     first_name: "Joe",
+  #     last_name:  "Tester",
+  #     email:      "joetester@example.com",
+  #     password:   "dottle-nouveau-pavilion-tights-furze"
+  #   )
+    
+  #   user.projects.create(
+  #     name: "Test Project"
+  #   )
+    
+  #   other_user = User.create(
+  #     first_name: "Jane",
+  #     last_name:  "Tester",
+  #     email:      "janetester@example.com",
+  #     password:   "dottle-nouveau-pavilion-tights-furze"
+  #   )
+    
+  #   other_project = other_user.projects.build(
+  #     name: "Test Project"
+  #   )
+    
+  #   expect(other_project).to be_valid
+  # end
+  
+  # it 'is invalid without a project name' do
+  #   user = User.create(
+  #     first_name: "Joe",
+  #     last_name:  "Tester",
+  #     email:      "joetester@example.com",
+  #     password:   "dottle-nouveau-pavilion-tights-furze"
+  #   )
+    
+  #   project = user.projects.new(name: nil)
+    
+  #   project.valid?
+    
+  #   expect(project.errors[:name]).to include("can't be blank")
+  # end
 end
