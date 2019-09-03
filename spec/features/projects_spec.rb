23023ef2d2e44e28679b0430c7c3e3fb5437dffe
gin <ginjirokimura0831@gmail.com>
2019-09-03 07:07:00 +0000

    Finish up to page 145

--- spec/features/projects_spec.rb
+++ spec/features/projects_spec.rb
@@ -1,6 +1,31 @@
 require 'rails_helper'
 
 RSpec.feature "Projects", type: :feature do
+  include LoginSupport
+  
+  # 8章 サポートモジュール
+  
+  scenario "user creates a new project" do
+    user = FactoryBot.create(:user)
+    
+    sign_in user
+    visit root_path
+    
+    expect {
+      click_link "New Project"
+      fill_in "Name", with: "Test Project"
+      fill_in "Description", with: "Trying out Capybara"
+      click_button "Create Project"
+      
+      expect(page).to have_content "Project was successfully created"
+      expect(page).to have_content "Test Project"
+      expect(page).to have_content "Owner: #{user.name}"
+    }.to change(user.projects, :count).by(1)
+  end
+  
+  # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  
+  # 普通
   
   # scenario "user creates a new project" do
   #   user = FactoryBot.create(:user)
--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -2,6 +2,50 @@ require 'rails_helper'
 
 RSpec.describe Note, type: :model do
   
+  # 8章 let
+  
+  let(:user) { FactoryBot.create(:user) }
+  let(:project) { FactoryBot.create(:project, owner: user) }
+  
+  it "is valid with a user, project, and message" do
+    note = Note.new(message: "This is a sample note", user: user, project: project)
+    expect(note).to be_valid
+  end
+  
+  it "is invalid without a message" do
+    note = Note.new(message: nil)
+    note.valid?
+    
+    expect(note.errors[:message]).to include("can't be blank")
+  end
+  
+  describe "search messagefor a term" do
+    let!(:note1) { FactoryBot.create(:note, project: project, user: user, message: "This is the first note.") }
+    let!(:note2) { FactoryBot.create(:note, project: project, user: user, message: "This is the second note.") }
+    let!(:note3) { FactoryBot.create(:note, project: project, user: user, message: "First, preheat the oven.")}
+    
+    context "when a match is found" do
+      it "returns notes that match the search term" do
+        expect(Note.search("first")).to include(note1, note3)
+      end
+    end
+    
+    context "when no match is found" do
+      
+      it "returns an empty collection" do
+        expect(Note.search("message")).to be_empty
+      end
+      
+      it "returns an empty collection" do
+        expect(Note.search("message")).to be_empty
+        expect(Note.count).to eq 3
+      end
+      
+    end
+  end
+  
+  # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  
   # it "generates associated date from a factory" do
   #   note = FactoryBot.create(:note)
     
